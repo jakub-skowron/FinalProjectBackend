d0c9118052167be9d7cd309aadf0646f87cd7fe6
Jakub Skowron <jakubmskowron@gmail.com>
2023-05-31 20:10:38 +0200

    Add new existsByName method

--- src/main/java/com/example/backend/repository/OrganizationRepository.java
+++ src/main/java/com/example/backend/repository/OrganizationRepository.java
@@ -4,7 +4,10 @@ import com.example.backend.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+    boolean existsByName(String string);
 
 }
