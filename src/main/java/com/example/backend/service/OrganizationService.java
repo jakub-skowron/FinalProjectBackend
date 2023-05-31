0f552edb396034b5c14665545198e38c79354b46
Jakub Skowron <jakubmskowron@gmail.com>
2023-05-31 21:59:21 +0200

    Add getOrganizationById method

--- src/main/java/com/example/backend/service/OrganizationService.java
+++ src/main/java/com/example/backend/service/OrganizationService.java
@@ -5,6 +5,7 @@ import com.example.backend.repository.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -17,10 +18,18 @@ public class OrganizationService {
         return new HashSet<>(organizationRepository.findAll());
     }
 
+    public Optional<Organization> getOrganizationById(Long id) {
+        if (organizationRepository.existsById(id)) {
+            return organizationRepository.findById(id);
+        }else{
+            throw new IllegalArgumentException("The Organization with inserted id doesn't exist");
+        }
+    }
+
     public void addOrganization(Organization organization) {
-        if(!organizationRepository.existsById(organization.getId()) && !organizationRepository.existsByName(organization.getName())){
+        if (!organizationRepository.existsById(organization.getId()) && !organizationRepository.existsByName(organization.getName())) {
             organizationRepository.save(organization);
-        }else throw new IllegalArgumentException("The Organization name or id already exists!");
+        } else throw new IllegalArgumentException("The Organization name or id already exists!");
     }
 
     public void removeOrganization(Long id) {
