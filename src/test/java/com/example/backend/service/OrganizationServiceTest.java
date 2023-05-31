b45a29b75a83932ad3649067bccb9a60ae7dc601
Jakub Skowron <jakubmskowron@gmail.com>
2023-05-31 11:52:37 +0200

    Fix 3 unit tests of addOrganization method  - try-catch block added

--- src/test/java/com/example/backend/service/OrganizationServiceTest.java
+++ src/test/java/com/example/backend/service/OrganizationServiceTest.java
@@ -2,18 +2,18 @@ package com.example.backend.service;
 
 import com.example.backend.model.Organization;
 import com.example.backend.repository.OrganizationRepository;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 
-@SpringBootTest
 class OrganizationServiceTest {
     @Mock
     OrganizationRepository organizationRepository;
@@ -35,4 +35,55 @@ class OrganizationServiceTest {
         organizationService.addOrganization(organization);
         verify(organizationRepository, times(1)).save(organization);
     }
+
+    @Test
+    public void addOrganizationWithTooShortNameShouldThrowException() {
+        long id = 1;
+        String shortName = "A"; // Name is too short (min = 2)
+        Organization organization = new Organization();
+        organization.setId(id);
+        organization.setName(shortName);
+
+        try {
+            organizationService.addOrganization(organization);
+        } catch (ConstraintViolationException e) {
+            assertTrue(e.getConstraintViolations().stream()
+                    .anyMatch(violation -> violation.getMessage().equals("size must be between 2 and 20")));
+        }
+    }
+
+    @Test
+    public void addOrganizationWithTooLongNameShouldThrowException() {
+        long id = 1;
+        String longName = "This name is too long"; // Name is too long (max = 20)
+        Organization organization = new Organization();
+        organization.setId(id);
+        organization.setName(longName);
+
+        try {
+            organizationService.addOrganization(organization);
+        } catch (ConstraintViolationException e) {
+            assertTrue(e.getConstraintViolations().stream()
+                    .anyMatch(violation -> violation.getMessage().equals("size must be between 2 and 20")));
+        }
+    }
+
+    @Test
+    public void addOrganizationWithEmptyNameShouldThrowException() {
+        long id = 1;
+        String emptyName = ""; // Name is an empty String
+        Organization organization = new Organization();
+        organization.setId(id);
+        organization.setName(emptyName);
+
+        try {
+            organizationService.addOrganization(organization);
+        } catch (ConstraintViolationException e) {
+            assertTrue(e.getConstraintViolations().stream()
+                    .anyMatch(violation -> violation.getMessage().equals("must not be blank")));
+            assertTrue(e.getConstraintViolations().stream()
+                    .anyMatch(violation -> violation.getMessage().equals("size must be between 2 and 20")));
+        }
+    }
+
 }
