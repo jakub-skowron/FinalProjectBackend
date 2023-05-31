6d8925deaeb2988c78d65a50046b52dd93cd4e9b
Jakub Skowron <jakubmskowron@gmail.com>
2023-05-31 20:15:21 +0200

    Fix addOrganization tests and add two new ones with testing addOrganization method with invalid id and name fields

--- src/test/java/com/example/backend/service/OrganizationServiceTest.java
+++ src/test/java/com/example/backend/service/OrganizationServiceTest.java
@@ -2,28 +2,39 @@ package com.example.backend.service;
 
 import com.example.backend.model.Organization;
 import com.example.backend.repository.OrganizationRepository;
-import jakarta.validation.ConstraintViolationException;
+import jakarta.annotation.Resource;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 class OrganizationServiceTest {
+    private Validator validator;
     @Mock
     OrganizationRepository organizationRepository;
 
+    @Resource
     @InjectMocks
     OrganizationService organizationService;
 
     @BeforeEach
     void onInit() {
         MockitoAnnotations.initMocks(this);
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
     }
 
     @Test
@@ -33,7 +44,7 @@ class OrganizationServiceTest {
         organization.setId(id);
         organization.setName("Valid Name"); // Name length = 10
         organizationService.addOrganization(organization);
-        verify(organizationRepository, times(1)).save(organization);
+        verify(organizationRepository).save(organization);
     }
 
     @Test
@@ -44,12 +55,13 @@ class OrganizationServiceTest {
         organization.setId(id);
         organization.setName(shortName);
 
-        try {
-            organizationService.addOrganization(organization);
-        } catch (ConstraintViolationException e) {
-            assertTrue(e.getConstraintViolations().stream()
-                    .anyMatch(violation -> violation.getMessage().equals("size must be between 2 and 20")));
-        }
+        Set<ConstraintViolation<Organization>> violations = validator.validate(organization);
+
+        assertEquals(1, violations.size());
+
+        ConstraintViolation<Organization> violation = violations.iterator().next();
+        assertEquals("size must be between 2 and 20", violation.getMessage());
+        assertEquals("name", violation.getPropertyPath().toString());
     }
 
     @Test
@@ -60,12 +72,13 @@ class OrganizationServiceTest {
         organization.setId(id);
         organization.setName(longName);
 
-        try {
-            organizationService.addOrganization(organization);
-        } catch (ConstraintViolationException e) {
-            assertTrue(e.getConstraintViolations().stream()
-                    .anyMatch(violation -> violation.getMessage().equals("size must be between 2 and 20")));
-        }
+        Set<ConstraintViolation<Organization>> violations = validator.validate(organization);
+
+        assertEquals(1, violations.size());
+
+        ConstraintViolation<Organization> violation = violations.iterator().next();
+        assertEquals("size must be between 2 and 20", violation.getMessage());
+        assertEquals("name", violation.getPropertyPath().toString());
     }
 
     @Test
@@ -76,14 +89,51 @@ class OrganizationServiceTest {
         organization.setId(id);
         organization.setName(emptyName);
 
-        try {
-            organizationService.addOrganization(organization);
-        } catch (ConstraintViolationException e) {
-            assertTrue(e.getConstraintViolations().stream()
-                    .anyMatch(violation -> violation.getMessage().equals("must not be blank")));
-            assertTrue(e.getConstraintViolations().stream()
-                    .anyMatch(violation -> violation.getMessage().equals("size must be between 2 and 20")));
-        }
+        Set<ConstraintViolation<Organization>> violations = validator.validate(organization);
+
+        assertEquals(2, violations.size());
+        assertTrue(violations.toString().contains("size must be between 2 and 20"));
+        assertTrue(violations.toString().contains("must not be blank"));
     }
 
+    @Test
+    public void addOrganizationWithoutNameShouldThrowException() {
+        long id = 1;
+        // Without setting name
+        Organization organization = new Organization();
+        organization.setId(id);
+
+        Set<ConstraintViolation<Organization>> violations = validator.validate(organization);
+
+        assertEquals(1, violations.size());
+        assertTrue(violations.toString().contains("must not be blank"));
+    }
+
+    @Test
+    public void addOrganizationWithExistingIdShouldThrowException() {
+        long id = 1;
+        Organization organization = new Organization();
+        organization.setId(id);
+        organization.setName("Valid Name");
+        when(organizationRepository.existsById(id)).thenReturn(true);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            organizationService.addOrganization(organization);
+        });
+    }
+
+    @Test
+    public void addOrganizationWithExistingNameShouldThrowException() {
+        long id = 1;
+        String existingName = "Existing Name";
+        Organization organization = new Organization();
+        organization.setId(id);
+        organization.setName(existingName);
+
+        when(organizationRepository.existsByName(existingName)).thenReturn(true);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            organizationService.addOrganization(organization);
+        });
+    }
 }
