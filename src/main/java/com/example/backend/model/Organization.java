cf6e631768d27d9c4e2080d2ee3bb7ea8bd99a90
Jakub Skowron <jakubmskowron@gmail.com>
2023-06-01 16:40:38 +0200

    Add new field (rooms) to Organization entity and set one to many relation

--- src/main/java/com/example/backend/model/Organization.java
+++ src/main/java/com/example/backend/model/Organization.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
@@ -23,4 +25,7 @@ public class Organization {
     @Size(min = 2, max = 20)
     @Column(unique = true)
     private String name;
+
+    @OneToMany(mappedBy = "organization")
+    private Set<Room> rooms;
 }
