6a4577f073f15385532fb1ad6039ae48c79cb1ef
Jakub Skowron <jakubmskowron@gmail.com>
2023-06-01 21:50:33 +0200

    Add RoomBuilder and 3 new test cases for addRoom method

---
+++ src/test/java/com/example/backend/service/RoomServiceTest.java
@@ -0,0 +1,92 @@
+package com.example.backend.service;
+
+import com.example.backend.model.Room;
+import com.example.backend.repository.RoomRepository;
+import jakarta.annotation.Resource;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+class RoomServiceTest {
+    private Validator validator;
+    private Room room;
+    private Map<Room.PlaceType, Integer> places;
+    @Mock
+    RoomRepository roomRepository;
+    @Resource
+    @InjectMocks
+    RoomService roomService;
+
+    @BeforeEach
+    void onInit() {
+        MockitoAnnotations.initMocks(this);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        places = new HashMap<>();
+        places.put(Room.PlaceType.SITTING, 3);
+        places.put(Room.PlaceType.STANDING, 4);
+        room = new Room.Builder()
+                .withId(1)
+                .withName("Valid Name")
+                .withIdentifier("Valid Identifier")
+                .withAvailability(true)
+                .withLevel(1)
+                .withPlaces(places)
+                .build();
+    }
+
+    @Test
+    public void addRoomWithValidPayloadShouldPass() {
+
+        Set<ConstraintViolation<Room>> violations = validator.validate(room);
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(0).isEqualTo(violations.size());
+        softly.assertAll();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"A, size must be between 2 and 20", "This name is too long, size must be between 2 and 20"})
+    public void addRoomWithInvalidNameShouldThrowException(String name, String expectedErrorMessage) {
+        room.setName(name);
+
+        Set<ConstraintViolation<Room>> violations = validator.validate(room);
+        ConstraintViolation<Room> violation = violations.iterator().next();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(1).isEqualTo(violations.size());
+        softly.assertThat(expectedErrorMessage).isEqualTo(violation.getMessage());
+        softly.assertThat("name").isEqualTo(violation.getPropertyPath().toString());
+        softly.assertAll();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"A, size must be between 2 and 20", "This name is too long, size must be between 2 and 20"})
+    public void addRoomWithInvalidIdentifierShouldThrowException(String identifier, String expectedErrorMessage) {
+        room.setIdentifier(identifier);
+
+        Set<ConstraintViolation<Room>> violations = validator.validate(room);
+        ConstraintViolation<Room> violation = violations.iterator().next();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(1).isEqualTo(violations.size());
+        softly.assertThat(expectedErrorMessage).isEqualTo(violation.getMessage());
+        softly.assertThat("identifier").isEqualTo(violation.getPropertyPath().toString());
+        softly.assertAll();
+    }
+}
