46df9c63e1b60a050751e00863c6ba7350289251
Jakub Skowron <jakubmskowron@gmail.com>
2023-06-04 10:28:14 +0200

    Add logs to OrganizationService

--- src/main/java/com/example/backend/service/OrganizationService.java
+++ src/main/java/com/example/backend/service/OrganizationService.java
@@ -6,15 +6,17 @@ import com.example.backend.model.Organization;
 import com.example.backend.model.Room;
 import com.example.backend.repository.OrganizationRepository;
 import com.example.backend.repository.RoomRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class OrganizationService {
+
+    Logger LOGGER = LoggerFactory.getLogger(OrganizationService.class);
 
     @Autowired
     OrganizationRepository organizationRepository;
@@ -23,53 +25,79 @@ public class OrganizationService {
     RoomRepository roomRepository;
 
     public List<Organization> getOrganizations() {
+        LOGGER.info("List all organizations");
         return organizationRepository.findAll();
     }
 
     public Organization getOrganizationById(Long id) {
+        LOGGER.info("Searching for organization with id {}", id);
         if (organizationRepository.existsById(id)) {
+            LOGGER.info("The organization with id {} was found", id);
             return organizationRepository.findById(id).get();
         } else {
-            throw new ObjectNotFoundException("The Organization with inserted id doesn't exist");
+            LOGGER.debug("The organization with id {} not found", id);
+            throw new ObjectNotFoundException("There is no organization with inserted id");
         }
     }
 
     public void addOrganization(Organization organization) {
-        if (!organizationRepository.existsById(organization.getId()) && !organizationRepository.existsByName(organization.getName())) {
-            organizationRepository.save(organization);
-        } else throw new ObjectAlreadyExistsException("The Organization name or id already exists!");
+        LOGGER.info("Adding organization");
+        if (organizationRepository.existsById(organization.getId())) {
+            LOGGER.debug("The organization with id {} already exists", organization.getId());
+            throw new ObjectAlreadyExistsException("The organization with inserted id already exists");
+        }
+        if (organizationRepository.existsByName(organization.getName())) {
+            LOGGER.debug("The organization with name {} already exists", organization.getName());
+            throw new ObjectAlreadyExistsException("The organization with inserted name already exists");
+        }
+        organizationRepository.save(organization);
+        LOGGER.info("The organization was saved");
     }
 
     public void removeOrganizationById(Long id) {
+        LOGGER.info("Deleting organization");
         if (organizationRepository.existsById(id)) {
+            LOGGER.info("The organization with id {} was found", id);
             organizationRepository.deleteById(id);
+            LOGGER.info("The organization was deleted");
         } else {
-            throw new ObjectNotFoundException("The Organization with inserted id doesn't exist");
+            LOGGER.debug("The organization with id {} not found", id);
+            throw new ObjectNotFoundException("There is no organization with inserted id");
         }
     }
 
     public void updateOrganizationNameById(long id, Organization organization) {
+        LOGGER.info("Updating organization");
         if (organizationRepository.existsById(id)) {
+            LOGGER.info("The organization with id {} was found", id);
+            LOGGER.info("Setting updated fields");
             organization.setId(id);
             organizationRepository.save(organization);
+            LOGGER.info("The organization was updated");
         } else {
-            throw new ObjectNotFoundException("The Organization with inserted id doesn't exist");
+            LOGGER.debug("The organization with id {} not found", id);
+            throw new ObjectNotFoundException("There is no organization with inserted id");
         }
     }
 
     public void addRoomToOrganization(long organizationId, long roomId) {
-        if (organizationRepository.existsById(organizationId) && roomRepository.existsById(roomId)) {
+        LOGGER.info("Adding room to organization");
+        if (!organizationRepository.existsById(organizationId)) {
+            LOGGER.debug("The organization with id {} not found", organizationId);
+            throw new ObjectNotFoundException("There is no organization with inserted id");
+        }
+        LOGGER.info("The organization with id {} was found", organizationId);
+        if (roomRepository.existsById(roomId)) {
+            LOGGER.info("The room with id {} was found", roomId);
             Organization organization = organizationRepository.findById(organizationId).get();
             Room room = roomRepository.findById(roomId).get();
-            if (room.isAvailability()) {
-                room.setAvailability(false);
-                room.setOrganization(organization);
-                roomRepository.save(room);
-            } else {
-                throw new IllegalArgumentException("The room is not available");
-            }
+            LOGGER.info("Setting room to organization");
+            room.setOrganization(organization);
+            roomRepository.save(room);
+            LOGGER.info("The room was added to organization");
         } else {
-            throw new ObjectNotFoundException("There is no organization or room with inserted id");
+            LOGGER.debug("The room with id {} not found", roomId);
+            throw new ObjectNotFoundException("There is no room with inserted id");
         }
     }
 }
